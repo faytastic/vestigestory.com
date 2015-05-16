4c3c4b74b67a3ee6c849cc4e8487de6a80645723
Andrew Wei <andrew@variante.io>
2015-05-15 17:40:07 -0700

    Fix port env variable

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.listen(process.env.port || 3000);
+app.listen(process.env.PORT || 3000);
