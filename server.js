3a556bdcf336d387f3f1ada5c69f16a7a644aa6f
Andrew Wei <andrew@variante.io>
2015-05-23 18:53:37 -0700

    Add expire headers

--- server.js
+++ server.js
@@ -3,8 +3,24 @@
  *  (c) Vestige <http://vestigestory.com>
  */
 
+'use strict';
+
 var express = require('express');
+var expiry = require('static-expiry');
 var app = express();
+
+app.use(function(req, res, next)
+{
+    if (req.url.indexOf('/assets/') === 0)
+    {
+        res.setHeader('Cache-Control', 'public, max-age=345600');
+        res.setHeader('Expires', new Date(Date.now() + 345600000).toUTCString());
+    }
+
+    return next();
+});
 
 app.use(express.static(__dirname + '/public'));
 app.listen(process.env.PORT || 9000);
+
+console.log('Listening on port ' + (process.env.PORT || 9000) + '...');
